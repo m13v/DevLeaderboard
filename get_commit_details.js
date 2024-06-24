f8721e37926af52fdd92991ba1b52467feaf6926
matthew-heartful <matthew.heartful@gmail.com>
2024-06-24 15:42:55 -0700

    fixed get call

--- get_commit_details.js
+++ get_commit_details.js
@@ -1,47 +1,30 @@
 const axios = require('axios');
 require('dotenv').config();
 
-async function getCommitDetails(owner, repo, ref, token) {
-    // GitHub API URL
-    const url = `https://api.github.com/repos/${owner}/${repo}/commits/${ref}`;
+async function getCommitDetails(commitUrl) {
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+        throw new Error("GITHUB_TOKEN environment variable not set");
+    }
 
-    // Headers
     const headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": `Bearer ${token}`
     };
 
     try {
-        // Make the request
-        const response = await axios.get(url, { headers });
+        const response = await axios.get(commitUrl, { headers });
         const commitData = response.data;
-        console.log(commitData);  // Add this line
-
-        // Print additions and deletions
-        commitData.files.forEach(file => {
-            const filename = file.filename;
-            const patch = file.patch;
-            console.log(`File: ${filename}`);
-            console.log(patch);
-            console.log("-".repeat(40));
-        });
+        // console.log('Commit Data:', JSON.stringify(commitData, null, 2)); // Pretty print the JSON
+        return commitData;
     } catch (error) {
         console.error('Error fetching commit details:', error);
+        throw error;
     }
 }
 
-function main() {
-    const githubToken = process.env.GITHUB_TOKEN;
-    if (!githubToken) {
-        throw new Error("GITHUB_TOKEN environment variable not set");
-    }
+module.exports = { getCommitDetails };
 
-    // Replace with your values
-    const owner = "matthew-heartful";
-    const repo = "DevLeaderboard";
-    const ref = "11cc5a970cb50acefd194cc8c82369192a4dc409";
-    getCommitDetails(owner, repo, ref, githubToken);
-}
-
-
-// main();
+// If you want to run this script directly, uncomment the following lines:
+// const commitUrl = "https://api.github.com/repos/matthew-heartful/DevLeaderboard/commits/dd0d56a2888efa8c51fb9a97fe495b3d7d102254";
+// getCommitDetails(commitUrl).then(data => console.log(data)).catch(err => console.error(err));
