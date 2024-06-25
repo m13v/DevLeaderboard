00cea901343facbab329c31ae69fc5a63e73f074
matthew-heartful <matthew.heartful@gmail.com>
2024-06-24 17:10:16 -0700

    loop to run until we process the entire queue

--- proceess_queue.js
+++ proceess_queue.js
@@ -1,22 +1,26 @@
 const { getCommitDetails } = require('./get_commit_details');
-const { getAllQueueContents, moveShaToCompleted } = require('./db_queue_mgt');
+const { get50QueueContents, moveCommitToCompleted } = require('./db_queue_mgt');
 
 
 async function processQueue() {
-    const queueContents = await getAllQueueContents();
-    console.log(queueContents);
+    let queueContents = await get50QueueContents();
 
-    for (const item of queueContents) {
-        const { commit_url } = item;
-        console.log(`commit_url: ${commit_url}`);
-        // break;
-        try {
-            console.log('getCommitDetails for commit=', commit_url);
-            const commitData = await getCommitDetails(commit_url);
-            await moveShaToCompleted(commit_url, commitData);
-        } catch (error) {
-            console.error(`Error processing commit ${commit_url}:`, error);
+    while (queueContents.length > 0) {
+        console.log(queueContents.length);   
+
+        for (const item of queueContents) {
+            const { commit_url } = item;
+            console.log(`commit_url: ${commit_url}`);
+            
+            try {
+                console.log('getCommitDetails for commit=', commit_url);
+                const commitData = await getCommitDetails(commit_url);
+                await moveCommitToCompleted(commit_url, commitData);
+            } catch (error) {
+                console.error(`Error processing commit ${commit_url}:`, error);
+            }
         }
+        queueContents = await get50QueueContents();
     }
 }
 
