38627be68269c41b3763b5c5e1c16c92713de4be
matthew-heartful <matthew.heartful@gmail.com>
2024-06-29 15:28:49 -0700

    handling rate limit delays modifications

--- process_repo_queue.js
+++ process_repo_queue.js
@@ -1,15 +1,15 @@
 const { get50Repos_AI_tag } = require('./db_repo_queue');
 const { fetchContributors } = require('./get_contributors');
 const { deleteRepoFromQueue } = require('./db_repo_queue');
-const { updateRepo, printRepoData } = require('./db_repos_data'); // Import the new function
+const { updateRepo } = require('./db_repos_data'); // Import the new function
 
 async function processRepos() {
     while (true) {
         try {
             const repos = await get50Repos_AI_tag();
             if (repos.length === 0) {
-                console.log('No repos fetched from the queue. Sleeping for 60 minutes...');
-                await new Promise(resolve => setTimeout(resolve, 60 * 60 * 1000));
+                console.log('No repos fetched from the queue. Sleeping for 20 minutes...');
+                await new Promise(resolve => setTimeout(resolve, 20 * 60 * 1000));
                 continue;
             }
             console.log(`Fetched ${repos.length} repos from the queue.`);
@@ -24,9 +24,9 @@ async function processRepos() {
                     await updateRepo(repo.repo_url, repo.fulldetails, repo.ai_tag); 
                     console.log(`Updated repo url and fulldetails`);
                 } catch (repoError) {
-                    if (repoError.response && repoError.response.status === 429) {
-                        console.error(`Rate limit hit. Sleeping for 60 minutes...`);
-                        await new Promise(resolve => setTimeout(resolve, 60 * 60 * 1000));
+                    if (repoError.response && (repoError.response.status === 429 || repoError.response.status === 403)) {
+                        console.error(`Rate limit or forbidden error hit. Sleeping for 20 minutes...`);
+                        await new Promise(resolve => setTimeout(resolve, 20 * 60 * 1000));
                     } else {
                         console.error(`Error processing repo ${repo.repo_url}:`, repoError);
                     }
@@ -34,8 +34,8 @@ async function processRepos() {
             }
 
             // Sleep for 60 minutes
-            console.log('Sleeping for 60 minutes...');
-            await new Promise(resolve => setTimeout(resolve, 60 * 60 * 1000));
+            console.log('Sleeping for 20 minutes...');
+            await new Promise(resolve => setTimeout(resolve, 20 * 60 * 1000));
         } catch (error) {
             console.error('Error fetching repos from the queue:', error);
         }
