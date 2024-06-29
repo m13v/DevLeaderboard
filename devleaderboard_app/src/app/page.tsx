8b84fd528f593b9dabf204028be9e025c3cedfe7
matthew-heartful <matthew.heartful@gmail.com>
2024-06-29 15:30:51 -0700

    rendering logic for test supabase function

--- devleaderboard_app/src/app/page.tsx
+++ devleaderboard_app/src/app/page.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import UsersTable from '../components/UsersTable';
+import SomeComponent from '../components/SearchField';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Open Source Developer Leaderboard</h1>
+      <SomeComponent />
       <UsersTable />
     </div>
   );
