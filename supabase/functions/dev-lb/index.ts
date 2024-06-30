4e1ead8ed3611acdc7d6ef7a50d08e9df06be1f5
matthew-heartful <matthew.heartful@gmail.com>
2024-06-30 11:12:19 -0700

    simplified function endpoint

--- supabase/functions/dev-lb/index.ts
+++ supabase/functions/dev-lb/index.ts
@@ -9,14 +9,19 @@ console.log("Hello from Functions!")
 
 import { serve } from 'https://deno.land/std@0.201.0/http/server.ts';
 import { handler1 } from './handlers/handler1.ts';
+import { lookupUserHandler } from './handlers/lookup_user_handler.ts';
 import { corsHeaders } from '../_shared/cors.ts';
 
 serve(async (req: Request) => {
+  console.log(`Received request: ${req.method} ${req.url}`); // Log request method and URL
+
   if (req.method === 'OPTIONS') {
+    console.log('Handling CORS preflight request');
     return new Response('ok', { headers: corsHeaders });
   }
 
-  return handler1(req);
+  console.log('Invoking handler1');
+  return lookupUserHandler(req);
 });
 
 /* To invoke locally:
@@ -24,9 +29,9 @@ serve(async (req: Request) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/hello-world' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/lookup-user' \
+    --header 'Authorization: Bearer YOUR_ANON_KEY' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"username":"exampleUser"}'
 
 */
